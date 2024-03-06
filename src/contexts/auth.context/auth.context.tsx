f6e28dbbbe52a43141927d1bc79e5204b6fe06b3
Jay <horanghwnii@gmail.com>
2024-03-06 16:55:24 +0900

    [UA-14] feat:  AuthContext default setting

---
+++ src/contexts/auth.context/auth.context.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import { createContext, useContext, useState } from "react";
+
+type AuthContextValue = {
+  isLoggedIn: boolean;
+  logIn: () => void;
+  logOut: () => void;
+};
+
+const initialValue: AuthContextValue = {
+  isLoggedIn: false,
+  logIn: () => {},
+  logOut: () => {},
+};
+
+const AuthContext = createContext(initialValue);
+
+export const useAuth = () => useContext(AuthContext);
+
+export function AuthProvider({ children }: { children: React.ReactNode }) {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const logIn = () => {
+    setIsLoggedIn(true);
+  };
+
+  const logOut = () => {
+    setIsLoggedIn(false);
+  };
+
+  const value = {
+    isLoggedIn,
+    logIn,
+    logOut,
+  };
+
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+}
