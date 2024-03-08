ce15f2e2afb62e3965fb5056103909cdd8676acc
Yujiii0 <yuji918@naver.com>
2024-03-09 03:37:37 +0900

    [UA-48] design:  starIcon 변경

--- src/app/(providers)/(root)/events/[eventId]/page.tsx
+++ src/app/(providers)/(root)/events/[eventId]/page.tsx
@@ -1,6 +1,6 @@
 import Heading from "@/components/Heading/Heading";
 import Page from "@/components/Page";
-import ReviewCardList from "@/components/ReviewCardList";
+import ReviewCard from "@/components/ReviewCard";
 
 async function EventDetailPage(props: { params: { eventId: string } }) {
   const eventId = props.params.eventId;
@@ -12,7 +12,8 @@ async function EventDetailPage(props: { params: { eventId: string } }) {
       타이틀, 공연 기간, 공연 장소, 출연진, 공연 런타임, 관람 연령, 티켓 가격,
       장르, 공연 상태, 썸네일 포스터, 지도 (주소 텍스트), 상세 이미지들, + 리뷰
       전체 정보
-      <ReviewCardList reviews={reviews}></ReviewCardList>
+      {/* <ReviewCardList reviews={reviews}></ReviewCardList> */}
+      <ReviewCard />
     </Page>
   );
 }
