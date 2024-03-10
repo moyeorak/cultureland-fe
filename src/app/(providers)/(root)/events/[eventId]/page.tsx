fc9761e78e0ea1537489611b44a64e17f6bcca25
Yujiii0 <yuji918@naver.com>
2024-03-10 14:20:20 +0900

    [UA-74] fix:  detailPage 수정

--- src/app/(providers)/(root)/events/[eventId]/page.tsx
+++ src/app/(providers)/(root)/events/[eventId]/page.tsx
@@ -1,16 +1,13 @@
-import Heading from "@/components/Heading/Heading";
 import Page from "@/components/Page";
-import ReviewForm from "./_components/ReviewForm";
+import DetailTabSection from "./_components/DetailTab/DetailTab";
+import EventDetailSection from "./_components/EventDetailSection";
 
 function EventDetailPage(props: { params: { eventId: string } }) {
   const eventId = Number(props.params.eventId);
   return (
     <Page>
-      <Heading label="EventDetailPage" />
-      타이틀, 공연 기간, 공연 장소, 출연진, 공연 런타임, 관람 연령, 티켓 가격,
-      장르, 공연 상태, 썸네일 포스터, 지도 (주소 텍스트), 상세 이미지들, + 리뷰
-      전체 정보
-      <ReviewForm eventId={eventId} />
+      <EventDetailSection />
+      <DetailTabSection eventId={eventId} />
     </Page>
   );
 }
