1b462eea21a520d63f7074aaca1bda8fac374092
Yujiii0 <yuji918@naver.com>
2024-03-12 10:09:04 +0900

    [UA-129] fix:  starIcon build 수정

--- src/app/(providers)/(root)/events/[eventId]/_components/StarIcon/StarIcon.tsx
+++ src/app/(providers)/(root)/events/[eventId]/_components/StarIcon/StarIcon.tsx
@@ -3,14 +3,24 @@ import Image from "next/image";
 interface IconProps {
   isHover?: boolean;
   path?: "Star-fill" | "Star-empty" | "Star-half";
+  size: "small" | "big";
 }
 
-function StarIcon({ isHover = false, path = "Star-empty" }: IconProps) {
+function StarIcon({ isHover = false, path = "Star-empty", size }: IconProps) {
   const imagePath = isHover
     ? "/utils/icons/star-fill.png"
     : `/utils/icons/${path}.png`;
 
-  return <Image src={imagePath} alt="star-icon" width={32} height={32} />;
+  const imageSize = size === "small" ? 24 : 32;
+
+  return (
+    <Image
+      src={imagePath}
+      alt="star-icon"
+      width={imageSize}
+      height={imageSize}
+    />
+  );
 }
 
 export default StarIcon;
