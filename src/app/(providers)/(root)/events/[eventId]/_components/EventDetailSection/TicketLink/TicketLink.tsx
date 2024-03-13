15baa71e57450240f0fc64fe7f0e25d2d6f19970
Yujiii0 <yuji918@naver.com>
2024-03-14 00:57:57 +0900

    [UA-162] fix:  TicketLink 수정

--- src/app/(providers)/(root)/events/[eventId]/_components/EventDetailSection/TicketLink/TicketLink.tsx
+++ src/app/(providers)/(root)/events/[eventId]/_components/EventDetailSection/TicketLink/TicketLink.tsx
@@ -1,15 +1,38 @@
+"use client";
+
+import { bookingLink } from "@/types/Event.type";
 import Image from "next/image";
 import Link from "next/link";
+import { useState } from "react";
 
-function TicketLink() {
-  const bookingLink = ["인터파크", "위메프", "쿠팡"];
+interface TicketLinkProps {
+  bookingLinks: bookingLink[];
+}
+
+function TicketLink({ bookingLinks }: TicketLinkProps) {
+  const [showDropdown, setShowDropdown] = useState(false);
+  const [currentImageSrc, setCurrentImageSrc] = useState(
+    "/utils/icons/white-down.png"
+  );
+
+  const toggleDropdown = () => {
+    setShowDropdown(!showDropdown);
+    setCurrentImageSrc(
+      !showDropdown
+        ? "/utils/icons/white-up.png"
+        : "/utils/icons/white-down.png"
+    );
+  };
 
   return (
-    <div className="group relative w-80 mx-auto mb-16 z-40">
-      <button className=" transition duration-200 rounded-lg overflow-hidden bg-user-theme-100 text-font-white text-center text-fs-14 w-full px-[10px] py-[10px] focus:outline-none">
+    <div className="group relative w-80 mx-auto mb-20 z-40 ">
+      <button
+        onClick={toggleDropdown}
+        className=" transition duration-200 rounded-lg overflow-hidden bg-user-theme-100 text-font-white text-center text-fs-14 w-full px-[10px] py-[10px] focus:outline-none"
+      >
         예매처
         <Image
-          src={"/utils/icons/white.png"}
+          src={currentImageSrc}
           alt="drop-down"
           width={24}
           height={24}
@@ -17,14 +40,18 @@ function TicketLink() {
         />
       </button>
 
-      <div className="opacity-0 group-hover:opacity-100 hover:opacity-0 absolute w-full mt-6 transition-opacity duration-300 transform group-hover:translate-y-0">
+      <div
+        className={`${
+          showDropdown ? "opacity-100" : "opacity-0"
+        } absolute w-full mt-6 transition-opacity duration-300 transform`}
+      >
         <ul className="flex flex-col shadow-primary rounded-md py-2 px-2 bg-white">
-          {bookingLink.map((link, index) => (
+          {bookingLinks.map((bookingLink, index) => (
             <li
               key={index}
               className="px-4 py-[15px] text-fs-14 text-font-40 hover:text-user-theme-100 transition cursor-pointer"
             >
-              <Link href={"#"}>{link}</Link>
+              <Link href={bookingLink.link}>{bookingLink.name}</Link>
             </li>
           ))}
         </ul>
