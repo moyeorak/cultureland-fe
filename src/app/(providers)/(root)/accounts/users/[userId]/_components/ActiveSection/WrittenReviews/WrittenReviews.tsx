284d039c40e4cbbfc5916488522a405947f48ee9
Yujiii0 <yuji918@naver.com>
2024-03-13 14:20:00 +0900

    [UA-162] feat:  WrittenReviews 생성

---
+++ src/app/(providers)/(root)/accounts/users/[userId]/_components/ActiveSection/WrittenReviews/WrittenReviews.tsx
@@ -0,0 +1,15 @@
+function WrittenReviews() {
+  //api
+
+  return (
+    <div>
+      {/* <ReviewCardList
+        reviews={reviews}
+        isGrid
+        eventId={reviews[0].eventId}
+      ></ReviewCardList> */}
+    </div>
+  );
+}
+
+export default WrittenReviews;
