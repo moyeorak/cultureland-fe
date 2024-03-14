17f391ec12d0b893973eb043b71d99c9a1a8eb40
Jay <horanghwnii@gmail.com>
2024-03-15 04:27:15 +0900

    [UA-231] feat:  현재 위치 기반 데이터 활용 아이템 리스트 시각화

--- src/components/KakaoMap/KakaoMap.tsx
+++ src/components/KakaoMap/KakaoMap.tsx
@@ -1,8 +1,10 @@
 "use client";
 
 import useQueryGetEventsOnMap from "@/react-query/reviews/useQueryGetEventsOnMap";
+import { Event } from "@/types/Event.type";
 import { useEffect, useState } from "react";
 import { Map, MapMarker } from "react-kakao-maps-sdk";
+import MapItemList from "../MapItemList";
 
 function KakaoMap() {
   const [center, setCenter] = useState({ lat: 37.568683, lng: 126.980279 });
@@ -20,28 +22,33 @@ function KakaoMap() {
   }, []);
 
   return (
-    <div className="w-[50%] h-[550px] overflow-hidden ">
-      <Map
-        center={center}
-        level={3}
-        style={{ width: "100%", height: "95%" }}
-        onIdle={(map) => {
-          setCenter({
-            lat: map.getCenter().getLat(),
-            lng: map.getCenter().getLng(),
-          });
-        }}
-      >
-        {data?.map((event) => (
-          <MapMarker
-            key={event.id}
-            position={{
-              lat: event.venue?.latitude as number,
-              lng: event.venue?.longitude as number,
-            }}
-          />
-        ))}
-      </Map>
+    <div className="flex w-svw">
+      <div className="w-[50%] h-[550px] overflow-hidden border-blue-700">
+        <Map
+          center={center}
+          level={3}
+          style={{ width: "100%", height: "95%" }}
+          onIdle={(map) => {
+            setCenter({
+              lat: map.getCenter().getLat(),
+              lng: map.getCenter().getLng(),
+            });
+          }}
+        >
+          {data?.map((event) => (
+            <MapMarker
+              key={event.id}
+              position={{
+                lat: event.venue?.latitude as number,
+                lng: event.venue?.longitude as number,
+              }}
+            />
+          ))}
+        </Map>
+      </div>
+      <div className="w-[50%] h-[550px]">
+        <MapItemList events={data as Event[]} />
+      </div>
     </div>
   );
 }
---
+++ src/components/MapItemCard/MapItemCard.tsx
@@ -0,0 +1,60 @@
+import { Event } from "@/types/Event.type";
+import { formatDate } from "@/utils/formatDate.utils";
+import Image from "next/image";
+import Link from "next/link";
+
+interface MapItemCardProps {
+  event: Event;
+}
+
+function MapItemCard({ event }: MapItemCardProps) {
+  const rating = Math.floor(Number(event.avgRating)) || 1;
+
+  return (
+    <Link
+      href={`/events/${event.id}`}
+      className="block hover:-translate-y-2 transition"
+    >
+      <article className="flex flex-col w-full">
+        {/* 이미지 */}
+        <section className="relative aspect-[3/4]">
+          <Image
+            src={event.poster}
+            fill
+            alt={event.title}
+            className="object-cover rounded-xl"
+            priority
+            sizes="100%"
+          />
+        </section>
+
+        {/* 콘텐츠 */}
+        <section className="py-3 px-2.5">
+          <div className="flex gap-x-1.5 items-center leading-none mb-2">
+            <div className="text-[10px] flex gap-x-0.5 items-center">
+              {Array(rating)
+                .fill(true)
+                .map((_, index) => (
+                  <span key={index}>⭐️</span>
+                ))}
+            </div>
+          </div>
+
+          <h6 className="text-[15px] leading-tight mb-2 overflow-hidden line-clamp-2 font-medium">
+            {event.title}
+          </h6>
+
+          <address className="text-xs not-italic text-neutral-70 block">
+            {event.venue?.name}
+          </address>
+
+          <time className="text-xs block text-neutral-40">
+            {formatDate(event.startDate)} ~ {formatDate(event.endDate)}
+          </time>
+        </section>
+      </article>
+    </Link>
+  );
+}
+
+export default MapItemCard;
