1464f0081d1d87412a091c8fa013c4aba4ef1b8c
Jay <horanghwnii@gmail.com>
2024-03-15 04:27:15 +0900

    [UA-231] feat:  현재 위치 기반 데이터 활용 아이템 리스트 시각화

---
+++ src/components/MapItemList/MapItemList.tsx
@@ -0,0 +1,20 @@
+import { ComponentProps } from "react";
+import MapItemCard from "../MapItemCard";
+
+interface MapItemListProps {
+  events: Array<ComponentProps<typeof MapItemCard>["event"]>;
+}
+
+function MapItemList({ events }: MapItemListProps) {
+  return (
+    <ul className="grid grid-cols-2 md:grid-cols-4 gap-x-5 gap-y-8">
+      {events?.map((event) => (
+        <li key={event.id} className="block">
+          <MapItemCard event={event} />
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+export default MapItemList;
