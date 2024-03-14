fddec0bced21ec8117205b7126db37774ba7777d
Jay <horanghwnii@gmail.com>
2024-03-15 05:30:03 +0900

    [UA-231] feat:  지도 중심 위치 기반 아이템 리스트 시각화

--- src/components/MapItemList/MapItemList.tsx
+++ src/components/MapItemList/MapItemList.tsx
@@ -5,31 +5,12 @@ interface MapItemListProps {
   events: Array<ComponentProps<typeof MapItemCard>["event"]>;
 }
 
-export type MapEvent = {
-  averagerating: string;
-  category: {
-    name: string;
-  };
-  id: number;
-  distance: number;
-  title: string;
-  venue: {
-    latitude: number;
-    longitude: number;
-    name: string;
-  };
-  venueId: number;
-  _count: {
-    reviews: number;
-  };
-};
-
 function MapItemList({ events }: MapItemListProps) {
   return (
     <ul className="grid grid-cols-2 md:grid-cols-1 gap-x-5 gap-y-8">
       {events?.map((event) => (
         <li key={event.id} className="block">
-          <MapItemCard event={event as MapEvent} />
+          <MapItemCard event={event} />
         </li>
       ))}
     </ul>
