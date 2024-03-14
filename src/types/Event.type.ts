66237922936c9e9a510be6c833f6e052f491ad6a
Jay <horanghwnii@gmail.com>
2024-03-14 10:17:09 +0900

    [UA-219] fix:  이벤트 관련 api 호출시 반환되는 이벤트 데이터들의 타입이 변경

--- src/types/Event.type.ts
+++ src/types/Event.type.ts
@@ -1,13 +1,8 @@
+import { Area } from "./Area.type";
+import { Category } from "./Category.type";
 import { Partner } from "./Partner";
 import { Review } from "./Review.type";
 import { Venue } from "./Venue.type";
-
-// export type Event = {
-//   data: {
-//     EventData: EventData[];
-//     totalEventsCnt: number;
-//   };
-// };
 
 export type ResponseData = {
   data: EventData;
@@ -18,7 +13,11 @@ export type EventData = {
   totalEventsCnt: number;
 };
 
-export type Events = {
+export type HomeEvents = {
+  events: HomeEvent[];
+};
+
+export type HomeEvent = {
   id: number;
   partnerId: number;
   title: string;
@@ -28,24 +27,36 @@ export type Events = {
   venueId: number;
   createdAt: string;
   updatedAt: string;
-  deletedAt: null | string;
+  deletedAt: null;
   apiId: string;
   areaCode: number;
   categoryCode: number;
-  venue: Venue;
-  category: Category;
-  area: Area;
-  avgRating: number;
+  venue: {
+    id: number;
+    name: string;
+    address: string;
+    latitude: number;
+    longitude: number;
+    apiId: string;
+  };
+  category: {
+    code: number;
+    value: string;
+    name: string;
+  };
+  area: {
+    code: number;
+    name: string;
+    value: string;
+  };
+  _count: {
+    reviews: number;
+    attendedUsers: number;
+    interestedUsers: number;
+  };
 };
 
-export type GetEventData = {
-  event: Event;
-  avgRating: string;
-};
-export type GetHomeEvent = {
-  event: Event[];
-};
-export interface Event {
+export type Event = {
   id: number;
   partnerId: number;
   title: string;
@@ -69,19 +80,27 @@ export interface Event {
   };
   reviews: Review[];
   avgRating?: string;
-}
+};
 
-interface Area {
-  code: number;
-  name: string;
-  value: string;
-}
-
-interface Category {
-  code: number;
-  name: string;
-  value: string;
-}
+export type Events = {
+  id: number;
+  partnerId: number;
+  title: string;
+  poster: string;
+  startDate: string;
+  endDate: string;
+  venueId: number;
+  createdAt: string;
+  updatedAt: string;
+  deletedAt: null | string;
+  apiId: string;
+  areaCode: number;
+  categoryCode: number;
+  venue: Venue;
+  category: Category;
+  area: Area;
+  avgRating: number;
+};
 
 interface EventDetail {
   eventId: number;
@@ -91,20 +110,15 @@ interface EventDetail {
   timeInfo: string;
   targetAudience: string;
   eventStatusCode: number;
-  bookingLinks: bookingLink[];
+  bookingLinks: any[];
   description_images: DescriptionImage[];
   eventStatus: EventStatus;
 }
-export interface DescriptionImage {
+
+interface DescriptionImage {
   id: number;
   eventId: number;
   imageUrl: string;
-}
-export interface bookingLink {
-  id: number;
-  eventId: number;
-  link: string;
-  name: string;
 }
 
 interface EventStatus {
