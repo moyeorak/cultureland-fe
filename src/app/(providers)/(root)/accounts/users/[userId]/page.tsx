433bd87c1d00c10647965dbc61b1ed9bb277946a
Y-junyoung <qq01050949256@gmail.com>
2024-03-15 11:27:17 +0900

    [UA-239] fix:  UserPage Fix\n

--- src/app/(providers)/(root)/accounts/users/[userId]/page.tsx
+++ src/app/(providers)/(root)/accounts/users/[userId]/page.tsx
@@ -1,34 +1,36 @@
+"use client";
+
+import { GetUserData } from "@/api/accounts/users/users.data";
 import api from "@/api/index.api";
 import Page from "@/components/Page";
-import LikedReviews from "./_components/ActiveSection/LikedReviews";
+import { useTabStore } from "@/zustand";
+import { useEffect, useState } from "react";
+import ActiveSection from "./_components/ActiveSection";
+import FollowSection from "./_components/FollowSection";
+import FollowTab from "./_components/FollowTab";
+import InfoTabs from "./_components/InfoTabs";
 import ProfileSection from "./_components/ProfileSection";
 
-async function UserPage(props: { params: { userId: number } }) {
-  const userId = Number(props.params.userId);
-  const user = await api.users.getUser(userId);
+function UserPage(props: { params: { userId: number } }) {
+  const userId = props.params.userId;
+  const { showFollows } = useTabStore();
+  const [user, setUser] = useState<GetUserData>();
 
+  useEffect(() => {
+    const fetchUser = async () => {
+      const userInfo = await api.users.getUser(userId);
+      setUser(userInfo);
+    };
+    fetchUser();
+  }, [userId]);
+
+  if (!user) return;
   return (
-    <Page title={user.userProfile.nickname}>
-      <div className="flex gap-x-9">
+    <Page>
+      {showFollows ? <FollowTab follows={user._count} /> : <InfoTabs />}
+      <div className="flex space-x-4 m-4">
         <ProfileSection user={user} />
-
-        <div>
-          {/* <WrittenReviews userId={userId} /> */}
-          <h2>좋아요한 리뷰</h2>
-          <LikedReviews userId={userId} />
-        </div>
-      </div>
-
-      <div>
-        본인인 경우 - 팔로잉 수, 팔로워 수, 팔로윙 리스트, 팔로워 리스트, 프로필
-        이미지, 닉네임, 관심 이벤트 목록, 내가 쓴 리뷰, 내가 관람한 목록, 내가
-        좋아요 누른 리뷰 목록, + 이메일 (프로필 수정을 위해)
-      </div>
-
-      <div>
-        타인인 경우 - 팔로잉 수, 팔로워 수, 팔로윙 리스트, 팔로워 리스트, 프로필
-        이미지, 닉네임, 해당 유저의 관심 이벤트 목록, 해당 유저가 쓴 리뷰, 해당
-        유저가 관람한 목록,
+        {showFollows ? <FollowSection userId={user.id} /> : <ActiveSection />}
       </div>
     </Page>
   );
