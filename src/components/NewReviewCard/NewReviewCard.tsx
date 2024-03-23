eb85ff16de97f66a6be1cffdeb816ec958f74152
Yujiii0 <yuji918@naver.com>
2024-03-24 02:07:51 +0900

    [153-feat] design:  bestReview css 수정

--- src/components/NewReviewCard/NewReviewCard.tsx
+++ src/components/NewReviewCard/NewReviewCard.tsx
@@ -6,6 +6,7 @@ import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { MouseEventHandler } from "react";
+import StarRating from "../ReviewCard/_components/StarRating";
 
 interface NewReviewCardProps {
   review: Review;
@@ -35,10 +36,12 @@ function NewReviewCard({ review }: NewReviewCardProps) {
             sizes="100%"
           />
         </div>
-        <div className="grid grid-cols-1 gap-y-4">
+        <div className="grid grid-cols-1 gap-y-4  w-full">
           <div className="grid grid-cols-1 gap-y-1">
             <h6 className="font-medium line-clamp-2 leading-tight text-[15px]">
-              {review.event.title}
+              {review.event.title.length > 25
+                ? `${review.event.title.slice(0, 25)}...`
+                : review.event.title}
             </h6>
             <address className="text-xs not-italic text-neutral-70 block">
               {review.event.venue.name}
@@ -49,8 +52,8 @@ function NewReviewCard({ review }: NewReviewCardProps) {
             </time>
           </div>
 
-          <div className="grid grid-cols-1 gap-y-1.5">
-            <div className="flex items-center justify-between gap-x-2">
+          <div className="grid grid-cols-1 gap-y-1.5  w-full">
+            <div className="flex items-center justify-between gap-x-2  w-full">
               <button
                 className="flex gap-x-2 items-center"
                 onClick={handleClickUser}
@@ -71,18 +74,16 @@ function NewReviewCard({ review }: NewReviewCardProps) {
                 </div>
               </button>
 
-              <div className="flex gap-x-1.5 items-center leading-none ml-auto">
-                <div className="text-[10px] flex gap-x-0.5 items-center">
-                  {Array(review.rating)
-                    .fill(true)
-                    .map((_, index) => (
-                      <span key={index}>⭐️</span>
-                    ))}
-                </div>
+              <div className="flex gap-x-1.5 items-center leading-none ml-auto ">
+                <StarRating rate={review.rating} />
               </div>
             </div>
 
-            <p className="text-sm text-neutral-90">{review.content}</p>
+            <p className="text-sm text-neutral-90 pt-1">
+              {review.content.length > 168
+                ? `${review.content.slice(0, 168)}...`
+                : review.content}
+            </p>
           </div>
         </div>
       </article>
